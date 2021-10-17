974e6f47d24123e3567a7091b64ab27868675dc8
성현Maria 김 <globalkmaria@gmail.com>
2021-10-17 19:20:16 +0900

    [add] get post values

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// 오늘 날짜 create post date로 초기값으로 만들기
+// date 바뀜에 따른 day 변화
+// 로컬 스토리지에 저장하기
+(function () {
+  // * POST 부분
+  const date = document.querySelector('#post__date');
+  const day = document.querySelector('#post__day');
+  const postSaveBtn = document.querySelector('#post__save');
   // ** input emoji 한개만 클릭
   const inputEmojis = document.querySelectorAll('.item__emoji');
   for (const inputEmoji of inputEmojis) {
@@ -26,6 +34,49 @@
     const newday = newDate.toLocaleDateString('en-US', { weekday: 'long' });
     day.innerHTML = newday.slice(0, 3);
   }
+  // ** 저장하기
+  postSaveBtn.addEventListener('click', savePost);
+  function savePost(e) {
+    e.preventDefault();
+    const post = getPostValue();
+    resetPost();
+  }
+  function getPostValue() {
+    const elements = document.querySelector('#post__form').elements;
+    let post = {
+      Breakfast: '',
+      Dinner: '',
+      Drinks: '',
+      Lunch: '',
+      NightSnack: '',
+      Snacks: '',
+      b_emoji: null,
+      c_icon: 0,
+      d_emoji: null,
+      l_emoji: null,
+      w_icon: 0,
+    };
+    for (const item of elements) {
+      switch (item.type) {
+        case 'text':
+          post[item.name] = item.value;
+          break;
+        case 'checkbox':
+          if (item.name === 'w_icon' || item.name === 'c_icon') {
+            if (item.checked === true) {
+              post[item.name] = post[item.name] + 1;
+            }
+          } else {
+            if (item.checked === true) {
+              post[item.name] = item.value;
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
   function resetPost() {
     const Postform = document.querySelector('#post__form');
     Postform.reset();
@@ -46,3 +97,9 @@
     day.innerHTML = todayDay;
   }
 
+  function start() {
+    setTodayDate();
+  }
+
+  window.addEventListener('load', start);
+})();
